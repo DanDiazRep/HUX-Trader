cf2201a8e9aeac52da07ab3208a752f98cff7da7
Tim Willaert <tim.willaert@outlook.com>
2022-11-14 15:41:11 +0100

    New background image

--- frontend/src/components/shared/Container.tsx
+++ frontend/src/components/shared/Container.tsx
@@ -1,15 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import BackgroundImg from "../../assets/trader_background.png"
 
 type Props = {
     children: JSX.Element
 }
 
 export const Container = ({children}: Props) =>{
-    const backgroundImage = "https://tinder.com/static/build/07eef8b101ca939c978a599f508aa955.webp"
     const { isAuthenticated } = useAuth0();
 
     return(
-        <div className="grow flex" style={{backgroundImage: isAuthenticated ? '' : `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.6)), url(${backgroundImage})`}}>
+        <div className="grow flex" style={{backgroundImage: isAuthenticated ? '' : `linear-gradient(rgba(0, 0, 0, 0.95), rgba(0, 0, 0, 0.75)), url(${BackgroundImg})`}}>
             {children}
         </div>
     );
