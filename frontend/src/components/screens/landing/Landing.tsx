f6798b0fe7a40a7cb60162bf5feead0e16e085a3
Tim Willaert <tim.willaert@outlook.com>
2022-11-16 22:25:29 +0100

    Added icon

--- frontend/src/components/screens/landing/Landing.tsx
+++ frontend/src/components/screens/landing/Landing.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import "../../../App.css";
+import { FaHandsHelping } from "react-icons/fa"
 
 // This is the home screen for non logged in users
 export const Landing = () =>{
@@ -7,7 +8,12 @@ export const Landing = () =>{
   return (
     <>
       <div className="absolute w-full py-5 px-8 flex items-center justify-between">
-        <h2 className="text-white font-bold text-3xl">Trader</h2>
+        <div className="flex items-center gap-3">
+          <div className="p-2 rounded-md bg-gradient-to-tr from-sky-500 to-indigo-500" >
+            <FaHandsHelping color="white" size={25}/>
+          </div>
+          <h2 className="text-white font-bold text-3xl pb-1">Trader</h2>
+        </div>
         <Link to="/login">
           <button className="py-2 px-8 rounded-full bg-white font-bold">Log In</button>
         </Link>
@@ -16,7 +22,7 @@ export const Landing = () =>{
         <div className="flex flex-col items-center justify-center min-h-full py-64">
           <h1 className="text-white font-sans text-8xl font-bold">Swipe and Trade</h1>
           <Link to="/login">
-            <button className="bg-gradient-to-r from-sky-500 to-indigo-500 bg-opacity-5 px-14 py-2 rounded-full font-semibold text-white mt-7 text-lg">
+            <button className="bg-gradient-to-tr from-sky-500 to-indigo-500 bg-opacity-5 px-14 py-2 rounded-full font-semibold text-white mt-7 text-lg">
               Get Started
             </button>
           </Link>
